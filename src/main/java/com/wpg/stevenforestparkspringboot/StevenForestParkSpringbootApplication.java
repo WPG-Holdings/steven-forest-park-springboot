03686dbf3d6606356f3607ef08df9d3c6a861755
viridis <viridis959@gmail.com>
2021-11-22 10:45:29 +0800

    First commit

---
+++ src/main/java/com/wpg/stevenforestparkspringboot/StevenForestParkSpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.wpg.stevenforestparkspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class StevenForestParkSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StevenForestParkSpringbootApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wpg/stevenforestparkspringboot/StevenForestParkSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wpg.stevenforestparkspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StevenForestParkSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
