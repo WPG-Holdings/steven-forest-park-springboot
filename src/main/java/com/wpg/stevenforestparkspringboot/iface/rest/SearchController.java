c4b0958fd8c4e106c307036a2aa7ccf49dc04339
viridis <viridis959@gmail.com>
2021-11-30 07:23:29 +0800

    Add hotKeywordCounts API

--- src/main/java/com/wpg/stevenforestparkspringboot/iface/rest/SearchController.java
+++ src/main/java/com/wpg/stevenforestparkspringboot/iface/rest/SearchController.java
@@ -4,11 +4,17 @@ import com.wpg.stevenforestparkspringboot.model.Detail;
 import com.wpg.stevenforestparkspringboot.model.Operation;
 import com.wpg.stevenforestparkspringboot.service.UploadService;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.TermQueryBuilder;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
@@ -24,7 +30,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -42,17 +50,20 @@ public class SearchController {
     @Autowired
     ElasticsearchOperations elasticsearchOperations;
 
-    @PostMapping("search")
-    public ResponseEntity<?> search() {
+    @Autowired
+    RestHighLevelClient client;
+
+    @GetMapping("query")
+    public ResponseEntity<?> query(@RequestParam(value = "fieldName") String fieldName,
+                                   @RequestParam(value = "value") String value,
+                                   @RequestParam(required = false, value = "page", defaultValue = "0") Integer page,
+                                   @RequestParam(required = false, value = "size", defaultValue = "10") Integer size) {
         try {
-            TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("question", "aiimport");
+            TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery(fieldName, value);
             NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
-            nativeSearchQueryBuilder.withQuery(termQueryBuilder);
+            nativeSearchQueryBuilder.withQuery(termQueryBuilder).withPageable(PageRequest.of(page, size));
             NativeSearchQuery nativeSearchQuery = nativeSearchQueryBuilder.build();
-            SearchHits<Operation> search = elasticsearchOperations.search(nativeSearchQuery, Operation.class, IndexCoordinates.of("op_index"));
-            for (SearchHit<Operation> hit: search){
-                System.out.println("Question = " + hit.getContent());
-            }
+            SearchHits<Operation> search = elasticsearchOperations.search(nativeSearchQuery, Operation.class);
 
             return new ResponseEntity<>(search, new HttpHeaders(), HttpStatus.OK);
         } catch (Exception e) {
@@ -78,5 +89,24 @@ public class SearchController {
             e.printStackTrace();
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("hotKeywordCounts")
+    public ResponseEntity<HashMap<String, String>> getFunctionCount() throws IOException {
+        SearchRequest searchRequest = new SearchRequest();
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.size(0);
+        TermsAggregationBuilder termsAggregationBuilder = AggregationBuilders.terms("function_counts").field("function.keyword");
+        searchSourceBuilder.aggregation(termsAggregationBuilder);
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+        Terms termsAggs = searchResponse.getAggregations().get("function_counts");
+        List<? extends Terms.Bucket> buckets = termsAggs.getBuckets();
+        HashMap<String, String> keywordCountHashMap = new HashMap<>();
+        for (Terms.Bucket bucket : buckets) {
+            Long counts = bucket.getDocCount()/2;
+            keywordCountHashMap.put(bucket.getKeyAsString(), counts.toString());
+        }
+        return ResponseEntity.ok().body(keywordCountHashMap);
     }
 }
